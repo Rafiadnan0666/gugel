87c3ec00e40c242fbe0f7644dfbb23d10a1ed06f
Rafiadnan0666 <fn234561@gmail.com>
2025-09-21 23:23:10 +0700

    fix the dash in the filename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,7 @@ import Image from 'next/image';
 export default function LandingPage() {
   return (
     <div>
-      <Head>
-        <title>Landing Page</title>
-      </Head>
-      <main>
-        <h1>Welcome to the Landing Page</h1>
-        <Image src="/path/to/image.jpg" alt="Description" width={500} height={300} />
-      </main>
+    
     </div>
   );
 }
