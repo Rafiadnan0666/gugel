bb73826b515eb6576925ed31ee9aa0f27d1af451
Rafiadnan0666 <fn234561@gmail.com>
2025-09-23 20:56:20 +0700

    no tawk

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,21 +34,6 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-NHXS030H6D');
           `}
         </Script>
-
-        {/* Tawk.to Live Chat */}
-        {/* <Script id="tawk-chat" strategy="afterInteractive">
-          {`
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/683d7dac69a4dc19177da764/1iso35r38';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        </Script> */}
       </body>
     </html>
   );
